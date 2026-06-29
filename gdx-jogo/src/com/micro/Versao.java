915fc3a08a0df6d8c2024e66defa87c1c3d8d25d
Shiniga-OP <shinigadev@gmail.com>
2026-06-29 12:26:26 -0300

    0.0.5 reserva

--- gdx-jogo/src/com/micro/Versao.java
+++ gdx-jogo/src/com/micro/Versao.java
@@ -5,9 +5,11 @@ public class Versao {
 	public static final int LISTAS = 0x02;
 	public static final int OTIMI1 = 0x03;
 	public static final int BPeO = 0x04;
+	public static final int Facili = 0x05;
+	public static final int ORGANI = 0x06;
     
     public static int atual() {
-        return BPeO;
+        return ORGANI;
     }
 	
 	public static String formatar(int versao) {
@@ -16,6 +18,8 @@ public class Versao {
 			case LISTAS: return "0.0.2";
 			case OTIMI1: return "0.0.3";
 			case BPeO: return "0.0.4";
+			case Facili: return "0.0.5";
+			case ORGANI: return "0.0.6";
 			default: return "Desconhecida";
 		}
 	}
